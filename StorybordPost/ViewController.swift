3e0ef570fc23d080bb374c988f9383033b689dae
Abduqahhor <95927081+AbduqaxxorSaidjonov@users.noreply.github.com>
2022-07-18 14:00:33 +0500

    Initial Commit

---
+++ StorybordPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorybordPost
+//
+//  Created by Abduqaxxor on 18/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
